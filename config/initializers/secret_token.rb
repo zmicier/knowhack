67407911419903e4f02aaaf2265dc7eb43fff6cb
Źmicier Hryškievič <volny.licvin@gmail.com>
2013-02-10 16:16:53 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knowhack::Application.config.secret_token = '643a3f21ee12d7276f59cddb93d2230a329f2d5444a740469f3a565910c7994867e32756422b0913cc40accd3f9b283e7346af302511249ebde81e1ffcaba589'
